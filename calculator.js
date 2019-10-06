afc048cf757873672b7bd2d85c152f3ba2ed32fd
eloise-mcintyre <eloise.mcintyre2@outlook.com>
2019-10-07 10:03:18 +1300

    remove unecessary variables for numbers in JS

--- calculator.js
+++ calculator.js
@@ -1,20 +1,6 @@
-let oneBtn = document.getElementById('one');
-let twoBtn = document.getElementById('two');
-let threeBtn = document.getElementById('three');
-let fourBtn = document.getElementById('four');
-let fiveBtn = document.getElementById('five');
-let sixBtn = document.getElementById('six');
-let sevenBtn = document.getElementById('seven');
-let eightBtn = document.getElementById('eight');
-let nineBtn = document.getElementById('nine');
-let zeroBtn = document.getElementById('zero');
 
 let decimalBtn = document.getElementById('decimal');
 let cancelBtn = document.getElementById('cancel');
-let divideBtn = document.getElementById('divide');
-let subtractBtn = document.getElementById('subtract');
-let addBtn = document.getElementById('add');
-let equalsBtn = document.getElementById('equals');
 let displayElement = document.getElementById('top-display');
 
 let numBtns = document.getElementsByClassName("num-btn");
@@ -94,81 +80,3 @@ for (let i = 0; i < oppBtns.length; i++) {
     oppBtns[i].addEventListener('click', calculateFunc)
 } 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let input = [];
-// let total = 0;
-// let temp = '';
-
-//let buttonArr = document.querySelectorAll("button");
-
-//for (i = 0; i < buttonArr.length; i++) {
-   // buttonArr[i].addEventListener('click', displayFunc)
-   // }
-
-
-
-//    document.addEventListener('click', function() {
-//     let val = document.getElementById("display").value
-//     document.getElementById("display").value = e.target.innerHTML;
-//     function isNumber(val) {
-    
-//         if (!isNaN(val) || val === '.') {
-//             temp += val;
-//             document.querySelector("display").val(temp.substring(0,10))
-//         } else if (val === 'C') {
-//             temp = '';
-//             document.querySelector("display").val('')
-//         } else if (val === 'x') {
-//             input.push(temp);
-//             input.push('*');
-//             temp = '';
-//         } else if (val === '/') {
-//             input.push(temp);
-//             input.push('/');
-//             temp = '';
-//         } else if (val === '=') {
-//             input.push(temp);
-//             let firstNum = Number(input[0]);
-//             for (var i = 1; i < entries.length; i++) {
-//                 let secondNum = Number(entries[i+1])
-//                 let mathOp = input[i];
-                
-//                 if (mathOp === '+') 
-//                 { firstNum += secondNum; 
-//                 } else if (mathOp === '-') { firstNum -= secondNum; 
-    
-//                 } else if (mathOp === '*') { firstNum *= secondNum; 
-//                 } else if (mathOp === '/') { firstNum /= secondNum; }
-                
-//                 i++;
-//               }
-        
-//          }
-//     }
-// })
-
-
-
-
-// function numberThings(val) {
-//     if (val === 'X') {
-//         input.push('*')
-//     }
-//     console.log(input);
-// }
-
-// console.log(typeof(input));
